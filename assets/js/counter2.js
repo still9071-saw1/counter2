08eeb22f1cf870fe7a1b53d993deddffd4b2ca07
still9071-saw1 <still9071@gmail.com>
2026-05-05 18:37:05 +0900

    アレンジに伴う変更と修正

--- assets/js/counter2.js
+++ assets/js/counter2.js
@@ -1,20 +1,41 @@
 (() => {
   const $counter = document.getElementById("js-counter");
+  const buttons = document.getElementsByClassName("js-button");
+  const resetButton = document.getElementById("js-reset-button");
 
-  const clickHandler = (e) => {
-    const $targetButton = e.currentTarget;
-    let currentCount = parseInt($counter.textContent);
-    if ($targetButton.textContent === "+") {
-      $counter.textContent = currentCount + 1;
+  // カウンター更新処理
+  const updateCounter = (delta) => {
+    let current = parseInt($counter.textContent, 10);
+    let next = current + delta;
+
+    // カウンターの値が0未満にならないようにする
+    // 10以下で-10を押したときに、-10ではなく0になるようにするため
+    if (next < 0) {
+      next = 0;
+    }
+
+    $counter.textContent = next;
+
+    if (next !== 0 && next % 10 === 0) {
+      $counter.classList.add("is-multiple-of-10");
     } else {
-      $counter.textContent = currentCount - 1;
+      $counter.classList.remove("is-multiple-of-10");
     }
+  };
+
+  // ボタン押下
+  const clickHandler = (e) => {
+    const delta = parseInt(e.currentTarget.dataset.delta, 10);
+    updateCounter(delta);
+  };
+
+  // イベント登録
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", clickHandler);
   }
-  for (let index = 0; index < document.getElementsByClassName(
-    "js-button").length; index++){
-        document.getElementsByClassName("js-button")[index]
-        .addEventListener("click" , (e) => clickHandler(e));
-    }
+
+  // リセットボタン押下
+  resetButton.addEventListener("click", () => {
+    $counter.textContent = "0";
+  });
 })();
-
-
--- assets/js/reset.js
+++ assets/js/reset.js
@@ -1,10 +1,11 @@
-((() => {
+(() => {
     const $counter = document.getElementById('js-counter');
 
-    const clickHandler = (e) => {
+    const clickHandler = () => {
         $counter.textContent = 0;
+        $counter.classList.remove("is-multiple-of-10"); 
     };
 
     document.getElementById('js-reset-button')
-        .addEventListener('click', (e) => clickHandler(e));
-})());
+        .addEventListener('click', clickHandler);
+})();
